bcb92c1b5949670729734312fd096574cf922906
Mikolaj Kida <mikolaj.kida@skyengine.ai>
2025-07-03 09:30:25 +0200

    feat: first implementation.

--- extract.py
+++ extract.py
@@ -4,6 +4,8 @@ import shutil
 import numpy as np
 
 from pathlib import Path
+
+from scipy import ndimage
 from scipy.ndimage import label
 from tqdm import tqdm
 
@@ -14,17 +16,27 @@ def extract_voronoi_from_single_ply(input_file: Path) -> None:
 
     shutil.copy(str(input_file), str(tmp_dir))
 
-    run_template = Path("run_template.sh").read_text()
+    run_template = Path(__file__).parent.joinpath("run_template.sh").read_text()
     run_template = run_template.replace("${INPUT_ROOT}", str(input_file.parent))
 
     subprocess.run(run_template, shell=True)
     shutil.rmtree(str(tmp_dir))
 
 
-def voronoi_boundary_to_voronoi_cell(boundary_file: Path) -> None:
+def voronoi_boundary_to_voronoi_cell(boundary_file: Path, udf_file: Path) -> None:
     boundaries = np.load(boundary_file)
+    features_path = np.load(udf_file)
 
-    labels, _ = label(boundaries, structure=np.ones((3,3,3), dtype=bool))
+    boundaries = np.logical_and(boundaries, features_path[..., 0] < 0.2)
+
+    structuring_element = ndimage.generate_binary_structure(rank=3, connectivity=1)
+
+    boundaries = ndimage.binary_dilation(boundaries, structure=structuring_element, iterations=3)
+    boundaries = ndimage.binary_erosion(boundaries, structure=structuring_element, iterations=3)
+
+    np.save(str(boundary_file), boundaries)
+
+    labels, _ = label(1-boundaries, structure=np.ones((3,3,3), dtype=bool))
 
     np.save(boundary_file.parent / "voronoi_cells.npy", labels)
 
@@ -41,5 +53,6 @@ if __name__ == "__main__":
         extract_voronoi_from_single_ply(input_point_cloud)
 
         voronoi_boundary_path = abc / "voronoi_boundaries.npy"
+        features_path = abc / "features.npy"
 
-        voronoi_boundary_to_voronoi_cell(voronoi_boundary_path)
+        voronoi_boundary_to_voronoi_cell(voronoi_boundary_path, features_path)
--- run_template.sh
+++ run_template.sh
@@ -1,17 +1,27 @@
 docker run -i -v ${INPUT_ROOT}:/shared --shm-size 64g --gpus all --rm --name nvdnet nvd_release:v0 /bin/zsh -c "
-    export DATA_ROOT=/shared
-    export ROOT_PATH=/root/NVDNet
-    
-    cd \"\${ROOT_PATH}\" && /opt/conda/bin/python generate_test_voronoi_data.py \"\${DATA_ROOT}\" 1
+    export LD_LIBRARY_PATH=/root/NVDNet/build/bvh-distance-queries/:/opt/conda/lib/:/usr/local/cuda/lib64:$LD_LIBRARY_PATH
 
-    \"\${ROOT_PATH}/build/src/prepare_evaluate_gt_feature/prepare_evaluate_gt_feature\" \"\${DATA_ROOT}/ndc_mesh\" \"\${DATA_ROOT}/feat/ndc\" --resolution 256
+    cd /root/NVDNet && /opt/conda/bin/python generate_test_voronoi_data.py /shared 1
 
-    cd \"\${ROOT_PATH}/python\" && /opt/conda/bin/python test_model.py dataset.root=\"\${DATA_ROOT}\" dataset.output_dir=\"\${DATA_ROOT}/voronoi\"
-    
+    /root/NVDNet/build/src/prepare_evaluate_gt_feature/prepare_evaluate_gt_feature /shared/ndc_mesh /shared/feat/ndc --resolution 256
+
+    cd /root/NVDNet/python && /opt/conda/bin/python test_model.py dataset.root=/shared dataset.output_dir=/shared/voronoi
+
+    cd /shared
+
+    touch test_ids.txt
+
+    echo input >> test_ids.txt
+
+    cd /root/NVDNet && /opt/conda/bin/python scripts/extract_mesh_batched.py /shared/voronoi /shared/mesh /shared/test_ids.txt
+
+    cp /shared/voronoi/input_pred.npy /shared/voronoi_boundaries.npy
+    cp /shared/voronoi/input_feat.npy /shared/voronoi_feat.npy
+
+    rm -R /shared/test_ids.txt
+    rm -R /shared/voronoi
     rm -R /shared/feat
     rm -R /shared/ndc_mesh
-    cp /shared/voronoi/input_pred.npy /shared/voronoi_boundaries.npy
-    chown $(id -u):$(id -g) /shared/voronoi_boundaries.npy
-    rm -R /shared/voronoi
-"
 
+    chown -R $(id -u):$(id -g) /shared/*
+"
